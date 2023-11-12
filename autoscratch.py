253ffe78981b420b21e191a4f0a266f2551aa634
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-11-12 13:52:56 +0100

    moved config to its own function

--- autoscratch.py
+++ autoscratch.py
@@ -7,6 +7,7 @@ if not geteuid() == 0:
     print("\033[38;2;255;0;0mERROR:\033[0m You are not root!")
     exit(1)
 
+
 def ok(msg):
     print(f'\033[38;2;0;255;0m{msg}\033[0m')
 
@@ -20,8 +21,14 @@ def error(msg):
 def info(msg):
     print(f'\033[38;2;100;100;100m{msg}\033[0m')
 
+
+if system("rm -rf build"):
+    ok("successfully cleaned build environment")
+else:
+    warning("Failed to clear build environment")
+
 def pkg_install(pkg_name, pkg_cmds):
-    print(f'executing install phase for package {pkg_name}')
+    info(f"beginning build+install phase for package: {pkg_name}")
     tmp_script = f'/tmp/{pkg_name}.pkgmgr'
     if not system(f"rm -f '{tmp_script}'"):
         ok("removed temp script")
@@ -40,29 +47,32 @@ def pkg_install(pkg_name, pkg_cmds):
     else:
         error(f"Failed to install {pkg_name}")
 
+def pkg_configure(FILENAME):
+    FILENAME = argv[-1]
+    info(f"reading file: {FILENAME}")
+    with open(FILENAME, "r") as file:
+        json = loads(file.read())
+    pkg_name:str = json["name"]
+    pkg_src:str = json["src"]
+    info(f"beginning configuration of package: {pkg_name}")
+    #print(type(pkg_src))
+    if pkg_src:
+        pkg_cmds:list[str] = [
+            "rm build", 
+            "mkdir build", 
+            "\n".join([f"wget {pkg} -O build/{pkg.split('/')[-1]}" for pkg in pkg_src]) if isinstance(pkg_src, list) else f"wget {pkg_src} -O build/{pkg_src.split('/')[-1]}", 
+            "cd build", 
+            json["decomp"] if "decomp" in json else f"tar xf {pkg_src[0].split('/')[-1]}" if isinstance(pkg_src, list) else f"tar xf {pkg_src.split('/')[-1]}", 
+            f"cd {pkg_name}"
+        ] + json["cmds"]
+    else:
+        pkg_cmds = json["cmds"]
+    return pkg_name, pkg_cmds
+
 match argv[1]:
     case "single":
         FILENAME = argv[-1]
-        info(f"reading file: {FILENAME}")
-        with open(FILENAME, "r") as file:
-            json = loads(file.read())
-
-        pkg_name:str = json["name"]
-        pkg_src:str = json["src"]
-        info(f"beginning configuration of package: {pkg_name}")
-        #print(type(pkg_src))
-        if pkg_src:
-            pkg_cmds:list[str] = [
-                "rm build", 
-                "mkdir build", 
-                "\n".join([f"wget {pkg} -O build/{pkg.split('/')[-1]}" for pkg in pkg_src]) if isinstance(pkg_src, list) else f"wget {pkg_src} -O build/{pkg_src.split('/')[-1]}", 
-                "cd build", 
-                json["decomp"] if "decomp" in json else f"tar xf {pkg_src[0].split('/')[-1]}", 
-                f"cd {pkg_name}"
-            ] + json["cmds"]
-        else:
-            pkg_cmds = json["cmds"]
-        info(f"beginning build+install phase for package: {pkg_name}")
+        pkg_name, pkg_cmds = pkg_configure(FILENAME)
         pkg_install(pkg_name, pkg_cmds)
 
     case "orderfile":
@@ -71,26 +81,9 @@ match argv[1]:
         with open(ORDERFILE, "r") as file:
             pkgs = file.read().split("\n")
         for pkg in pkgs:
-            info(f"reading file: {pkg}")
-            with open(pkg, "r") as file:
-                json = loads(file.read())
-            pkg_name:str = json["name"]
-            pkg_src = json["src"]
-            info(f"beginning configuration of package: {pkg_name}")
-            if pkg_src:
-                pkg_cmds:list[str] = [
-                    "rm build",
-                    "mkdir build",
-                    "\n".join([f"wget {pkg} -O build/{pkg.split('/')[-1]}" for pkg in pkg_src]) if isinstance(pkg_src, list) else f"wget {pkg_src} -O build/{pkg_src.split('/')[-1]}", 
-                    "cd build",
-                    f"rm -f {pkg_name}",
-                    json["decomp"] if "decomp" in json else f"tar xf {pkg_src[0].split('/')[-1]}",
-                    f'cd {pkg_name}'
-                ] + json["cmds"]
-            else:
-                pkg_cmds = json["cmds"]
-            info(f"beginning build+install phase for package: {pkg_name}")
+            pkg_name, pkg_cmds = pkg_configure(pkg)
             pkg_install(pkg_name, pkg_cmds)
+
 
 
 
