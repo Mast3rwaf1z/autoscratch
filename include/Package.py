eb434f8426d0f610a295a7c3876fd522626f2cdc
Mast3r_waf1z <thomasmollerjensen@live.dk>
2023-11-13 20:33:48 +0100

    fixed tarball removal

--- include/Package.py
+++ include/Package.py
@@ -24,6 +24,7 @@ class Package:
     def configure(self):
         filename = f"/tmp/{self.name}-config.sh"
         check_call(["rm", "-f", filename])
+        check_call(["rm", "-f", f'{self.src}*']) if not isinstance(self.src, list) else [check_call(["rm", "-f", f'{src}*']) for src in self.src]
         with open(filename, "a") as file:
             file.write("cd build\n")
             if not self.src:
